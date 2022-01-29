f41c6d486657329397f17c9cc509dced1ed933bc
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2022-01-28 22:24:12 -0500

    Used JavaScript to bump the most recent note to the top

--- src/App.js
+++ src/App.js
@@ -28,13 +28,31 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        // Put the most recently-modified note at the top
+        setNotes(oldNotes => {
+            const newArray = []
+            for(let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
     }
+        
     
+        //.map doesn't allow for rearranging the notes
+        /*setNotes(oldNotes => oldNotes.map(oldNote => {
+        return oldNote.id === currentNoteId
+            ? { ...oldNote, body: text }
+            : oldNote
+    }))*/
+
+
+  
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
